de7f0f1d289af28bea9b18e65f51e20d4b67047f
Nikhita Mogili <69315352+nikhitamogili@users.noreply.github.com>
2021-10-19 11:42:07 +0530

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -55,33 +55,3 @@
   
 </body>
 </html>
-
-<?php
-   include('db.php');
-  
-   
-   if($_SERVER["REQUEST_METHOD"] == "POST") {
-      // username and password sent from form 
-      
-      $myusername = mysqli_real_escape_string($con,$_POST['user']);
-      $mypassword = mysqli_real_escape_string($con,$_POST['pass']); 
-      
-      $sql = "SELECT id FROM login WHERE usname = '$myusername' and pass = '$mypassword'";
-      $result = mysqli_query($con,$sql);
-      $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
-      $active = $row['active'];
-      
-      $count = mysqli_num_rows($result);
-      
-      // If result matched $myusername and $mypassword, table row must be 1 row
-		
-      if($count == 1) {
-         
-         $_SESSION['user'] = $myusername;
-         
-         header("location: home.php");
-      }else {
-         echo '<script>alert("Your Login Name or Password is invalid") </script>' ;
-      }
-   }
-?>
