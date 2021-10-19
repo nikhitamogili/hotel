97effb4a98507dd5e1fb22e3b36ab9a8d3a71bb4
Nikhita Mogili <69315352+nikhitamogili@users.noreply.github.com>
2021-10-19 11:01:29 +0530

    Update reservation.php

--- admin/reservation.php
+++ admin/reservation.php
@@ -1,8 +1,4 @@
-<?php
-include('db.php')
-?>
-
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!DOCTYPE html xmlns="http://www.w3.org/1999/xhtml">
 <head>
       <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
